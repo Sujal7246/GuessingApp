bef3508239a2b449d71a1ff62cbbd73355c33308
sujal sachdeva <sujalsachdeva0001@gmail.com>
2026-02-06 10:56:27 +0530

    UC4 Exception Handling done

--- src/main/java/GuessingApp.java
+++ src/main/java/GuessingApp.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingApp {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InvalidInputException{
        System.out.println("WELCOME TO GUESSING GAME");
        GameConfig gameConfig=new GameConfig();
        gameConfig.showRules();
@@ -10,21 +10,28 @@ public class GuessingApp {
        int attempts=0;
        int hintsUsed=0;
        while (attempts< gameConfig.getMAX_ATTEMPTS()){
-           System.out.println("Enter your guess");
-           int guess=sc.nextInt();
-           attempts++;
-           String result=GameValidator.validateGuess(guess,gameConfig.getTargetNumber());
-           if (!result.equals("CORRECT")
-                   && hintsUsed < gameConfig.getMAX_HINTS()) {
-               hintsUsed++;
-               System.out.println(
-                       HintService.generateHint(
-                               gameConfig.getTargetNumber(), hintsUsed)
-               );
+           try {
+               System.out.println("Enter your guess");
+               String input=sc.nextLine();
+               int guess = ValidationService.validateInput(input);
+               attempts++;
+               String result = GameValidator.validateGuess(guess, gameConfig.getTargetNumber());
+               if (!result.equals("CORRECT")
+                       && hintsUsed < gameConfig.getMAX_HINTS()) {
+                   hintsUsed++;
+                   System.out.println(
+                           HintService.generateHint(
+                                   gameConfig.getTargetNumber(), hintsUsed)
+                   );
+               }
+               System.out.println(result);
+               if ("CORRECT".equals(result)) {
+                   System.out.println("YOU WIN 🏆");
+                   break;
+               }
            }
-           System.out.println(result);
-           if("CORRECT".equals(result)){
-               break;
+           catch (InvalidInputException e){
+               System.out.println("ERROR: "+e.getMessage());
            }
        }
    }
